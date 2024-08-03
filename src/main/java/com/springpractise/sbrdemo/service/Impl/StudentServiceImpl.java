48dd586493097219e50656e60470eb70f242c5e6
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-08-03 18:57:26 +0530

    Fourth Commit-Exception Added

--- src/main/java/com/springpractise/sbrdemo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/springpractise/sbrdemo/service/Impl/StudentServiceImpl.java
@@ -2,46 +2,56 @@ package com.springpractise.sbrdemo.service.Impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springpractise.sbrdemo.exception.StudentAlreadyExistsException;
 import com.springpractise.sbrdemo.model.Student;
+import com.springpractise.sbrdemo.repository.StudentRepository;
 import com.springpractise.sbrdemo.service.IStudentservice;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Service
+@RequiredArgsConstructor //This Annotation is used to inject dependencies through constructor
 public class StudentServiceImpl implements IStudentservice{
+    
+    @Autowired
+    private final StudentRepository studentRepository;
 
     @Override
     public Student addStudent(Student student) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addStudent'");
+
+        if(studentAlreadyExists(student.getEmail())){
+            throw new StudentAlreadyExistsException(student.getEmail()+"Already Exsists!!!");
+        }
+        return studentRepository.save(student);
     }
 
+    
     @Override
     public List<Student> getStudents() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getStudents'");
+        return studentRepository.findAll();
     }
-
+    
     @Override
     public Student updateStudent(Student student, Long id) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updateStudent'");
     }
-
+    
     @Override
     public Student getStudentById(Long id) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getStudentById'");
     }
-
+    
     @Override
     public void deleteStudent(Long id) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deleteStudent'");
     }
 
-
-
-
+    //Implementing Methods....!!!
+    private boolean studentAlreadyExists(String email) {
+        return studentRepository.findByEmail(email).isPresent();
+    }
 }
