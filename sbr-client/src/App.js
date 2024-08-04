542d786efc53e34c3230bf86dbaa3afbf9200eb7
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-08-04 17:42:57 +0530

    Completed CRUD Operations using java FSD With React....!

--- sbr-client/src/App.js
+++ sbr-client/src/App.js
@@ -3,7 +3,9 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/common/NavBar";
 import AddStudent from "./components/student/AddStudent";
+import EditStudent from "./components/student/EditStudent";
 import StudentView from "./components/student/StudentView";
+import StudentPofile from "./components/student/StuentProfile";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -17,6 +19,12 @@ function App() {
      <Route exact path="/" element={<Home />}></Route>
      <Route exact path="/view-students" element={<StudentView />}></Route>
      <Route exact path="/add-students" element={<AddStudent />}></Route>
+     <Route exact path="/edit-student/:id" element={<EditStudent />}></Route>
+     <Route
+      exact
+      path="/student-profile/:id"
+      element={<StudentPofile />}
+     ></Route>
     </Routes>
    </Router>
   </main>
